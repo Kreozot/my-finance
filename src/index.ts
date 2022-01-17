37b240ed96652f18ac4f36173c77bab4028dd74c
Kreozot <zzxyyz@gmail.com>
2022-01-17 20:44:27 +0300

    TS

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,15 @@ const loadFile = async (path: string) => {
   return String(file);
 };
 
-const parseOfx = async (ofxString) => {
+const parseOfx = async (ofxString: string) => {
   return ofx.parse(ofxString);
 };
 
 const loadOfx = async (path: string) => {
   const ofxFile = await loadFile(path);
   const ofx = await parseOfx(ofxFile);
-  await fsExtra.writeFile(JSON.stringify(ofx, null, 2));
+  await fsExtra.ensureDir('out');
+  await fsExtra.writeFile('out/out.json', JSON.stringify(ofx, null, 2));
 };
+
+loadOfx('./data/operations Sat Jan 01 13_28_08 MSK 2022-Mon Jan 17 10_35_44 MSK 2022.ofx');
