b9adb276b2209c84eafb88acf273f51b9585c88c
Kreozot <zzxyyz@gmail.com>
2022-01-18 20:05:14 +0300

    Начало парсинга

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import inquirer from 'inquirer';
 import inquirerFuzzyPath from 'inquirer-fuzzy-path';
 import ofx from 'ofx';
 import fsExtra from 'fs-extra';
+import path from 'path';
 
 inquirer.registerPrompt('fuzzypath', inquirerFuzzyPath);
 
-const loadFile = async (path: string) => {
-  const file = await fsExtra.readFile(path);
+const loadFile = async (filePath: string) => {
+  const file = await fsExtra.readFile(filePath);
   return String(file);
 };
 
@@ -14,11 +15,57 @@ const parseOfx = async (ofxString: string) => {
   return ofx.parse(ofxString);
 };
 
-const loadOfx = async (path: string) => {
-  const ofxFile = await loadFile(path);
+const loadOfx = async (filePath: string) => {
+  const ofxFile = await loadFile(filePath);
   const ofx = await parseOfx(ofxFile);
-  await fsExtra.ensureDir('out');
-  await fsExtra.writeFile('out/out.json', JSON.stringify(ofx, null, 2));
+  return ofx;
 };
 
-loadOfx('./data/operations Sat Jan 01 13_28_08 MSK 2022-Mon Jan 17 10_35_44 MSK 2022.ofx');
+const chooseFile = async () => {
+  const { filePath } = await inquirer
+    .prompt([
+      {
+        type: 'fuzzypath',
+        name: 'filePath',
+        message: 'Select OFX file',
+        itemType: 'file',
+        excludeFilter: (nodePath: string) => !/\.ofx$/.test(nodePath),
+      },
+    ]) as any;
+  console.log(`Выбранный файл: ${filePath}`);
+  return path.join(filePath);
+};
+
+const parseDateTime = (dateTimeStr: string) => {
+  return dateTimeStr;
+};
+
+const parseAmount = (amountStr: string) => {
+  return parseInt(amountStr, 10);
+};
+
+const processOfx = (ofx: any) => {
+  const BANKTRANLIST = ofx.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST;
+  const startDate = parseDateTime(BANKTRANLIST.DTSTART);
+  const endDate = parseDateTime(BANKTRANLIST.DTEND);
+  const transactions = BANKTRANLIST.STMTTRN.map((STMTTRN: any) => {
+    return {
+      date: parseDateTime(STMTTRN.DTPOSTED),
+      amount: parseAmount(STMTTRN.TRNAMT),
+      name: STMTTRN.NAME,
+      category: STMTTRN.MEMO,
+      currency: STMTTRN.CURRENCY.CURSYM,
+    };
+  });
+  return transactions;
+};
+
+const start = async () => {
+  const filePath = await chooseFile();
+  const ofx = await loadOfx(filePath);
+  const transactions = processOfx(ofx);
+  await fsExtra.ensureDir('out');
+  await fsExtra.writeFile('out/transactions.json', JSON.stringify(transactions, null, 2));
+};
+
+start();
