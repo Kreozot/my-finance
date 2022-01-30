c41f9bc609782b4a4f0f39e17fa7faafa81c032a
Kreozot <zzxyyz@gmail.com>
2022-01-30 12:49:02 +0300

    Получение данных

--- src/googleApi.ts
+++ src/googleApi.ts
@@ -18,22 +18,51 @@ const authenticateSheets = async () => {
     auth: authClient
   });
 }
+class GoogleSheet {
+  sheets: Awaited<ReturnType<typeof authenticateSheets>>;
+  spreadsheetId: string;
+
+  constructor(spreadsheetId = GOOGLE_SHEET_ID) {
+    this.spreadsheetId = spreadsheetId;
+  }
+
+  async authenticate() {
+    this.sheets = await authenticateSheets();
+  }
+
+  async getCategoryColumns() {
+    const res = await this.sheets.spreadsheets.values.get({
+      spreadsheetId: GOOGLE_SHEET_ID,
+      range: 'Лист1',
+      majorDimension: 'COLUMNS',
+    });
+    console.log(res.data);
+  }
+}
+
+
 
 export const exportData = async () => {
-  const sheets = await authenticateSheets();
-  console.log(1);
-  const spreadsheet = await sheets.spreadsheets.values.append({
-    spreadsheetId: GOOGLE_SHEET_ID,
-    range: 'Лист1',
-    valueInputOption: 'USER_ENTERED',
-    requestBody: {
-      values: [
-        [
-          'test'
-        ]
-      ]
-    }
-  });
+  const googleSheet = new GoogleSheet();
+  await googleSheet.authenticate();
+  await googleSheet.getCategoryColumns();
+  // const sheets = await authenticateSheets();
+  // console.log(1);
+  // const range = await sheets.spreadsheets.values.get({
+
+  // })
+  // const spreadsheet = await sheets.spreadsheets.values.update({
+  //   spreadsheetId: GOOGLE_SHEET_ID,
+  //   range: 'Лист1',
+  //   valueInputOption: 'USER_ENTERED',
+  //   requestBody: {
+  //     values: [
+  //       [
+  //         'test'
+  //       ]
+  //     ]
+  //   }
+  // });
 
   console.log(2);
 }
