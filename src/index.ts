c40e1214d52404a30e3ed61951f07d13796972f6
Kreozot <zzxyyz@gmail.com>
2022-02-19 11:15:01 +0300

    Загрузка всех файлов

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,27 @@
-import inquirer from 'inquirer';
-import inquirerFuzzyPath from 'inquirer-fuzzy-path';
 import fsExtra from 'fs-extra';
 import path from 'path';
 import {
   isEqual, find, unionBy,
 } from 'lodash';
 import getHash from 'object-hash';
+import FileHound from 'filehound';
 
 import {
   DataProvider, Transaction,
 } from './types';
 import { TinkoffCsvDataProvider } from './providers/tinkoffCsv';
-import { TinkoffOfxDataProvider } from './providers/tinkoffOfx';
 import { SberbankPdfDataProvider } from './providers/sberbankPdf';
 import { AlfabankCsvDataProvider } from './providers/alfabankCsv';
 
-inquirer.registerPrompt('fuzzypath', inquirerFuzzyPath);
-
-const FILE_NAME_REGEXP = /\.(ofx|csv|pdf)$/;
-
-const chooseFile = async () => {
-  const { filePath } = await inquirer
-    .prompt([
-      {
-        type: 'fuzzypath',
-        name: 'filePath',
-        message: 'Выберите OFX, PDF или CSV файл',
-        itemType: 'file',
-        rootPath: 'data/',
-        excludeFilter: (nodePath: string) => !FILE_NAME_REGEXP.test(nodePath),
-      },
-    ]) as any;
-  return path.join(filePath);
-};
-
 const getDataProvider = async (filePath: string): Promise<DataProvider> => {
   switch (path.extname(filePath)) {
-    case '.ofx':
-      console.log('Tinkoff OFX');
-      return new TinkoffOfxDataProvider();
     case '.csv':
       if (await TinkoffCsvDataProvider.checkFile(filePath)) {
         console.log('Tinkoff CSV');
         return new TinkoffCsvDataProvider();
       }
       if (await AlfabankCsvDataProvider.checkFile(filePath)) {
-        console.log('Alfabank OFX');
+        console.log('Alfabank CSV');
         return new AlfabankCsvDataProvider();
       }
       throw new Error('Неизвестный формат CSV-файла');
@@ -53,7 +29,7 @@ const getDataProvider = async (filePath: string): Promise<DataProvider> => {
       console.log('Sberbank PDF');
       return new SberbankPdfDataProvider();
     default:
-      throw new Error('Поддерживаются только OFX, PDF или CSV файлы');
+      throw new Error('Поддерживаются только PDF или CSV файлы');
   }
 };
 
@@ -116,26 +92,43 @@ const addHash = (transactions: Transaction[]): Transaction[] => {
   return transactions;
 };
 
-const start = async () => {
-  const filePath = await chooseFile();
+const getFilesList = async (): Promise<string[]> => {
+  const files: string[] = await FileHound.create()
+    .paths('data/')
+    .ext(['csv', 'pdf'])
+    .find();
+  return files;
+};
+
+const getTransactionsFromFile = async (filePath: string): Promise<Transaction[]> => {
   const dataProvider = await getDataProvider(filePath);
-  console.log('Загрузка предыдущих данных');
-  const existingTransactions = await loadExistingTransactions();
   console.log('Парсинг файла', filePath);
   const newTransactions = await dataProvider.getDataFromFile(filePath);
   addHash(newTransactions);
   console.log('Поиск и фильтрация возвращённых транзакций');
   const { transactions, duplicates } = filterRevertTransactions(newTransactions);
-  console.log('Объединение данных');
-  const allTransactions = unionBy(existingTransactions, transactions, 'hash');
 
   console.log('Сохранение данных');
+  const name = path.basename(filePath);
   await fsExtra.ensureDir('out');
   await Promise.all([
-    fsExtra.writeFile('out/duplicates.json', JSON.stringify(duplicates, null, 2)),
-    fsExtra.writeFile('out/transactions.json', JSON.stringify(transactions, null, 2)),
-    fsExtra.writeFile('out/allTransactions.json', JSON.stringify(allTransactions, null, 2)),
+    fsExtra.writeFile(`out/duplicates-${name}.json`, JSON.stringify(duplicates, null, 2)),
+    fsExtra.writeFile(`out/transactions-${name}.json`, JSON.stringify(transactions, null, 2)),
   ]);
+  return transactions;
+};
+
+const start = async () => {
+  const files = await getFilesList();
+  console.log(files);
+  const transactionArrays = await Promise.all(files.map((filePath) => getTransactionsFromFile(filePath)));
+  console.log('Загрузка предыдущих данных');
+  let allTransactions = await loadExistingTransactions();
+  console.log('Объединение данных');
+  transactionArrays.forEach((transactions) => {
+    allTransactions = unionBy(allTransactions, transactions, 'hash');
+  });
+  await fsExtra.writeFile('out/allTransactions.json', JSON.stringify(allTransactions, null, 2));
 };
 
 start();
